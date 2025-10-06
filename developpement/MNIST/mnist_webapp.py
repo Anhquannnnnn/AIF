5dc48a76744aa191b093f45810670f57d8cec3de
Anhquannnnnn <anhquanletu@gmail.com>
2025-10-06 15:37:51 +0200

    update

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -2,15 +2,22 @@ import gradio as gr
 from PIL import Image
 import requests
 import io
-
+import numpy as np
 
 def recognize_digit(image):
     # Convert to PIL Image necessary if using the API method
-    image = Image.fromarray(image.astype('uint8'))
-    img_binary = ...
-    ...
-    ...
-    return ....
+    image = image['composite']
+    image = image[:,:,0]
+    image = abs(255-image)
+    image = image.astype(np.uint8)
+
+    # Convert the image to a PIL Image
+    image = Image.fromarray(image)
+    img_binary = io.BytesIO()
+    image.save(img_binary, format="PNG")
+    response = requests.post("http://127.0.0.1:5000/predict", data=img_binary.getvalue())
+    print(response.json()["prediction"])
+    return response.json()["prediction"]
 
 if __name__=='__main__':
 
@@ -19,4 +26,4 @@ if __name__=='__main__':
                 outputs='label',
                 live=True,
                 description="Draw a number on the sketchpad to see the model's prediction.",
-                ).launch(debug=True, share=True);
+                ).launch(debug=True, share=True)
